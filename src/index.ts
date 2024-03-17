49f954a889ba729352721d8d1b5d73a19b0202e6
stabble-devops <keisuke@stabble.org>
2024-03-17 14:11:45 -0400

    chore: bump npm packages

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,57 @@
 import { Program, Provider } from "@coral-xyz/anchor";
 import { PublicKey, TransactionInstruction } from "@solana/web3.js";
-import {
-  getAssociatedTokenAddressSync,
-  TOKEN_PROGRAM_ID,
-} from "@solana/spl-token";
-import { TransactionWithRecentBlock, WalletContext } from "@stabbleorg/solana-common";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { TransactionWithRecentBlock, WalletContext } from "@stabbleorg/anchor-contrib";
 import { SplFaucet, IDL } from "../target/types/spl_faucet";
 
 export class Faucet<T extends Provider> extends WalletContext<T> {
   static AUTHORITY_PREFIX = "Faucet Authority";
   readonly program: Program<SplFaucet>;
 
-  constructor(
-    provider: T,
-    programId?: PublicKey,
-  ) {
+  constructor(provider: T, programId?: PublicKey) {
     super(provider);
-    this.program = new Program(IDL, programId || new PublicKey("CCyXskW2kpYFHtLnJ8i8RNKJazD871iq3FhRSkbztjTm"), provider);
+    this.program = new Program(
+      IDL,
+      programId || new PublicKey("CCyXskW2kpYFHtLnJ8i8RNKJazD871iq3FhRSkbztjTm"),
+      provider,
+    );
   }
 
   get authorityAddress(): PublicKey {
     return PublicKey.findProgramAddressSync([Buffer.from(Faucet.AUTHORITY_PREFIX)], this.program.programId)[0];
   }
 
+  findAssociatedTokenAddress(mintAddress: PublicKey): PublicKey {
+    return this.getAssociatedTokenAddress(mintAddress, this.authorityAddress);
+  }
+
+  findDoneAddress(mintAddress: PublicKey): PublicKey {
+    return PublicKey.findProgramAddressSync(
+      [this.walletAddress.toBuffer(), mintAddress.toBuffer()],
+      this.program.programId,
+    )[0];
+  }
+
   async claim(mintAddress: PublicKey): Promise<TransactionWithRecentBlock> {
-    const ixs: TransactionInstruction[] = [];
+    const instructions: TransactionInstruction[] = [];
 
-    const faucetTokenAddress = getAssociatedTokenAddressSync(mintAddress, this.authorityAddress, true);
     const userToken = await this.getOrCreateAssociatedTokenAddressInstruction(mintAddress);
-    if (userToken.instruction) ixs.push(userToken.instruction);
+    if (userToken.instruction) instructions.push(userToken.instruction);
 
-    ixs.push(
+    instructions.push(
       await this.program.methods
         .claim()
         .accounts({
-          faucetToken: faucetTokenAddress,
+          faucetToken: this.findAssociatedTokenAddress(mintAddress),
           userToken: userToken.address,
           user: this.walletAddress,
           faucetAuthority: this.authorityAddress,
-          done: PublicKey.findProgramAddressSync(
-            [this.walletAddress.toBuffer(), mintAddress.toBuffer()],
-            this.program.programId,
-          )[0],
+          done: this.findDoneAddress(mintAddress),
           tokenProgram: TOKEN_PROGRAM_ID,
         })
         .instruction(),
     );
 
-    return this.newTX(ixs);
+    return this.newTX(instructions);
   }
 }
